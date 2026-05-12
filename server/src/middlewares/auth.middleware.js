919999055c885df0829f4b7c8faf19b437483cfe
Somayeh <somayehgulistani@gmail.com>
2026-05-12 18:31:45 +0430

    Implement authentication and RBAC system

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (...requiredRoles) => {
+  return (req, res, next) => {
+    try {
+      const token = req.headers.authorization;
+
+      if (!token) {
+        return res.status(401).json({
+          success: false,
+          message: "Unauthorized access",
+        });
+      }
+
+      // verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = decoded;
+
+      // role checking
+      if (requiredRoles.length && !requiredRoles.includes(decoded.role)) {
+        return res.status(403).json({
+          success: false,
+          message: "Forbidden access",
+        });
+      }
+
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token",
+      });
+    }
+  };
+};
+
+module.exports = auth;
--- server/src/modules/auth/auth.routes.js
+++ server/src/modules/auth/auth.routes.js
@@ -2,10 +2,36 @@ const express = require("express");
 
 const authController = require("./auth.controller");
 
+const auth = require("../../middlewares/auth.middleware");
+
 const router = express.Router();
 
 router.post("/register", authController.register);
 
 router.post("/login", authController.login);
+
+router.get("/admin-dashboard", auth("admin"), (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome Admin",
+    user: req.user,
+  });
+});
+
+router.get("/instructor-dashboard", auth("instructor"), (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome Instructor",
+    user: req.user,
+  });
+});
+
+router.get("/student-dashboard", auth("student"), (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome Student",
+    user: req.user,
+  });
+});
 
 module.exports = router;
